4606d99d93b0e3e5a3ba965c82dad729ceb1afcb
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-11-08 15:46:42 +0100

    Update 08_11_2018.py

--- 08_11_2018.py
+++ 08_11_2018.py
@@ -12,17 +12,21 @@ zapis.write("6 linia tekstu\n")
 zapis.close()#zamknięcie pliku obowiązkowo
 
 #otwieranie i wyświetlenie pliku
-zapis=open("plik3.txt", "r") #otwieramy plik3.txt
+zapis=open("plik3.txt", "r") 
+#otwieramy plik3.txt z parametrem „r” – Odczyt danych z pliku tekstowego.
 zawartosc= zapis.read() # podstawienie  pliku zapis do zmiennej zawartosc i odczyt
 print (zawartosc)#wydruk na ekran zawartosci pliku
 zapis.close()#zamknięcie pliku obowiązkowo
 
 #dodawanie danych do pliku
-zmiana=open("plik1.txt", "a")#otwieramy plik1.txt
+zmiana=open("plik1.txt", "a")
+#otwieramy plik1.txt z parametrem „a” – Dopisanie danych do pliku.
+# Jeśli plik nie istnieje, zostaje stworzony.
 zmiana.write("dodajemy cyfry: \n 1233 \n67879")#dodajemy linie np:z cyframi
 zmiana.close()#zamykamy plik
 
 #ponowne wyświetlenie zawartości plik1.txt
-f=open("plik1.txt", "r")#otwieramy plik1.txt
+f=open("plik1.txt", "r")
+#otwieramy plik1.txt z parametrem „r” – Odczyt danych z pliku tekstowego.
 print(f.read())#odczyt i wyświetlenie w jednej linii
 f.close()#zamknięcie pliku
