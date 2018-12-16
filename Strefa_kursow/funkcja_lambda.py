86e2dff0fe01c0528c07aed9aa4df052463e0fbe
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-12-16 17:32:59 +0100

    Add files via upload

---
+++ Strefa_kursow/funkcja_lambda.py
@@ -0,0 +1,7 @@
+"""
+Funkcja jednorazowa
+"""
+podwajanie = lambda a: a * 2
+potegowanie = lambda a: a * a
+print(podwajanie(7))#14
+print(potegowanie(7))#49
---
+++ Strefa_kursow/funkcje.py
@@ -0,0 +1,74 @@
+"""
+-bloki kodu do wielokrotnego wykorzystania
+-zestaw deklaracji które często się powtarzają
+-kluczowe elementy języka
+-liczne funkcje wbudowane oraz możliwość definiowania własnych
+def nazw_funkcji(argumenty):
+    deklaracje
+"""
+def suma(a,b):
+    print(a + b)
+suma(3,4)#7 musimy podać dwa argumenty w nawiasie brak jednego błąd programu
+suma(23443,435)#23878
+#aby uniknąć błedu nie wpisania argument funcji
+# domyślna wartość argumentu
+# mozna zdefiniować argument lub argumenty
+def suma(a=2, b=3):
+    print(a + b)
+suma()# 5
+suma(7)# 10
+suma(4,9)# 13
+# suma(,5) taki zapis wyrzuci błąd, musimy użyć tego spsobu zapisu:
+suma(b=17)# 19
+#suma(2, c=1, b=6) jeśli mamy więcej argumentów można stosować ten sposób
+# zapisu, nie musimy pisać a=2 bo 2 będzie odczytana z pierwszej pozycji jako a
+
+def moja_funkcja(a, *krotka, **slownik):#mozna podać tylko jedną wartość zostanie dopisan do a
+    # później możemy podać dowolne wartości które zosatną wpisane w krotke
+    # poźniej możemy podać klucze z ich wartościami zostaną wpisane w słownik
+    # należy pamiętać o kolejności podawanych wartości nie można wymieszać wartości krotek i słownika
+    print(a)
+    print(krotka)
+    print(slownik)
+moja_funkcja(3.14, "wdw", 23, 11, True, klucz1=21, klucz2=22, klucz3="domek")
+"""
+3.14
+('wdw', 23, 11, True)
+{'klucz1': 21, 'klucz2': 22, 'klucz3': 'domek'}
+"""
+#można użyć pętli for do wypisanie wartości krotki i słownika:
+def moja_2_funkcja(a, *krotka, **slownik):
+    print(a)
+    for i in krotka:
+        print(i)
+    for klucz in slownik:
+        print(slownik[klucz])
+moja_2_funkcja(3.14, "wdw", 23, 11, True, klucz1=21, klucz2=22, klucz3="domek")
+"""
+3.14
+wdw
+23
+11
+True
+21
+22
+domek
+"""
+
+def dodawanie(a,b):
+    return a+b, a*b, a-b # zwróc sume, iloczyn i różnice liczb a i b
+    # wyniki zostaną podane w postacji krotki
+    # koniec programu dodawanie, napisanie czegoś poniżej nie uruchomi się
+x = dodawanie(3,4)
+print(x)#wyświetli trzy wyniki w krotce (7, 12, -1)
+print(x[2])#-1 Tylko pozycja nr 2 krotki x
+
+#zmienne globalne i lokalne
+
+x = 1#zmienna globalna
+def moja_funkcja():
+    x = 2#zmienne lokalne
+    y = 3
+    print(x,y)
+moja_funkcja()#2 3
+print(x) #1wywołanie tylko zmiennej globalnej
