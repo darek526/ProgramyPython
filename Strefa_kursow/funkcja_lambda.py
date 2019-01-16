5f0a6f0b0a019fdf09af2052d6260546ae4030d9
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2019-01-16 21:27:32 +0100

    Update funkcja_lambda.py

--- Strefa_kursow/funkcja_lambda.py
+++ Strefa_kursow/funkcja_lambda.py
@@ -5,3 +5,14 @@ podwajanie = lambda a: a * 2
 potegowanie = lambda a: a * a
 print(podwajanie(7))#14
 print(potegowanie(7))#49
+
+#funkcja lambda jako element listy
+lista=[(lambda  a:a*5),(lambda a:a*6),(lambda a:a*7)]
+for i in lista:
+    print(i(3))
+"""
+15
+18
+21
+"""
+print(lista[1](3))#wypisuje tylko pozycje nr 1 z listy#18
