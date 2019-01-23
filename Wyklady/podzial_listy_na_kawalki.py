3f2d1a4b435a389dcdd67122adf4c567cc2925b2
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2019-01-24 00:16:47 +0100

    Update podzial_listy_na_kawalki.py

--- Wyklady/podzial_listy_na_kawalki.py
+++ Wyklady/podzial_listy_na_kawalki.py
@@ -8,11 +8,12 @@ def podziel_liste(calosc, dlugosc):
         kawalki.append(kawalek)
         if len(kawalek)<4:
             for i in range(4 - (len(kawalek))):
-                kawalek.append(0)
+                kawalek.append(0)#uzupełnienie listy do odpowiedniej ilości elementów 
     return kawalki
+#print(podziel_liste(lista,4))#wyswietli macierz w nawiesie trzeba wpisać nazwę listy do edycji i ilość elementów
 for kawalek in podziel_liste(lista, 4):#w nawiasie nazwa listy do podziału i ilość elementów
     # w każdej nowej liście liczba musi być taka sama jak przy warunku if powyżej i pętli for
-    print(kawalek)
+    print(kawalek)#wyswietli kilka list jedna pod drugą
 #https://pl.python.org/forum/index.php?action=printpage;topic=6219.0
 """
 #blok = lista[:4]
