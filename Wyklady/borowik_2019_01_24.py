f11ca27742c7f850ba81968e4996472f6c2f2629
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2019-01-24 18:07:11 +0100

    Update borowik_2019_01_24.py

--- Wyklady/borowik_2019_01_24.py
+++ Wyklady/borowik_2019_01_24.py
@@ -1,4 +1,38 @@
+"""
+logowanie do wirtualnego środowiska
+"""
+"""
+class Klasa(object):#Klasa dziedziczy po object
+    def __init__(self):# inicjalizcja funkcja która jest uruchamiana self
+        self.n = 5 # pole klasy zmienna lokalna
+    def funkcja(self, parametr):#self inf dla interpretera że jest to funkcja tej klasy
+        self.n = parametr
+    def drukuj(self):#wydruk na ekran
+        print(self.n)
+    def zwieksz(self):
+        self.n = self.n + 2
+    @staticmethod#dekorator
+    def metoda_statyczna(parametr):#dotyczy wszystkich obiektów m i k
+        print("Parametr: " + str(parametr))
 
+print(type(k))#sprawdzenie typu wynik <class '__main__.Klasa'>
+k = Klasa()
+k.drukuj()
+k.metoda_statyczna(10)
+
+m = Klasa()
+m.zwieksz()
+m.drukuj()
+k.drukuj()
+m.metoda_statyczna(7)
+"""
+5
+Parametr: 10
+7
+5
+Parametr: 7
+"""
+"""
 """
 polimorfizm
 """
@@ -55,7 +89,8 @@ def funkcja(x,y,*param):
 a,b=funkcja(5,3,5,7,3,2,3,4,5)
 print(a)
 print(b)
-"""[2, 6, 9]
+"""
+[2, 6, 9]
 5
 3
 [5, 7, 3, 2, 3, 4, 5]
