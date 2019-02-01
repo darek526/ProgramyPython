ce403233487e66dd17a360da6610ecd4bf91a9ce
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2019-02-01 14:08:00 +0100

    Create main.py

---
+++ TechAkademiaPl/main.py
@@ -0,0 +1,11 @@
+import modul_bmi#moduł stworzony przez nas w innym pliku tu go tylko wywołujemy
+import math#moduły wbudowane w Pythonie math to różne funkcje matematyczne
+
+modul_bmi.bmi("Darek",1.81,99)
+print(math.cos(30))#0.15425144988758405
+"""
+Marek Twoje bmi wynosi:  26.861206922865602
+#wykorzystuje dane zawarte w module
+Darek Twoje bmi wynosi:  30.218857788223804
+#wykorzystuje dane wpisane przy wywołanie modułu linia 4
+"""
