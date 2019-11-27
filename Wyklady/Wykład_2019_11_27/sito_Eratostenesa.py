ea2c57e6e10a2080d0c44ee54546f8697eaf2d6f
darek526 <32923506+darek526@users.noreply.github.com>
2019-11-27 12:17:15 +0100

    Update sito_Eratostenesa.py

--- Wyklady/Wykład_2019_11_27/sito_Eratostenesa.py
+++ Wyklady/Wykład_2019_11_27/sito_Eratostenesa.py
@@ -1,2 +1,22 @@
 #wyszukuje wszystkie liczby pierwsze z podanego przedziału
+# przy dużych liczbach wolno pracuje
+# nie działa
 import math
+a=[0,0]
+k=int(input("wpisz liczbę maksymalną k= "))
+for n in range(2,k+1): a.append(1)#dodanie samych 1
+for n in range(2,int(math.sqrt(k))):
+    if a[n]==1:
+        i=2*n
+        while i<=k:
+            a[i]=0
+            i=i+n
+ilosc_liczb=0
+for i in range(k+1):
+    if a[i]==1:
+        print("liczba",i,"jest liczbą pierwszą")
+        ilosc_liczb=ilosc_liczb+1
+print("ilość liczb pierwszych w tym przedziale wynosi",ilosc_liczb)
+print("gęstość teoretyczna", (math.log(k))/k)
+print("gęstość eksperymenralna",ilosc_liczb/k)
+print((math.log(math.e)))
